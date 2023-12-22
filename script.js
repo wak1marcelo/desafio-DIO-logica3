d14a092e79b57711217c316436e88a32cfe4d254
Marcelo Waki <wakimarcelo@outlook.com>
2023-12-21 22:53:22 -0300

    Codigo do desafio

---
+++ script.js
@@ -0,0 +1,35 @@
+class hero {
+  constructor(nameHero, ageHero, typeHero) {
+    this.nameHero = nameHero
+    this.ageHero = ageHero
+    this.typeHero = typeHero
+  }
+
+  atacar() {
+    let ataque = " ";
+    switch (this.typeHero) {
+      case 'mago':
+        ataque = "usou magia"
+        break
+      case 'guerreiro':
+        ataque = "usou a espada"
+        break
+      case 'monge':
+        ataque = "usou artes marciais"
+        break
+      case 'ninja':
+        ataque = "usou shuriken"
+        break
+      default:
+        ataque = "não atacou"
+        break
+    }
+    console.log(`O Heroi ${this.nameHero} tem ${this.ageHero} e é do tipo ${this.typeHero} atacou usando ${ataque}`)
+  }
+}
+
+let hero1 = new hero("Marcelo", 28, "mago")
+hero1.atacar()
+
+let hero2 = new hero("Lucian", 30, "guerreiro")
+hero2.atacar()
